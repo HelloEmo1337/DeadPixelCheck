e2d28286dabd1042421b38da18c080b1f42c07dd
HelloEmo1337 <84297457+HelloEmo1337@users.noreply.github.com>
2022-04-07 00:28:24 +0300

    src
    source

---
+++ emo.py
@@ -0,0 +1,15 @@
+import turtle as t
+playerAscore=3
+playerBscore=4
+ 
+window=t.Screen()
+window.title("Yarrag")
+window.bgcolor("Red")
+window.setup(width=1200,height=800)
+window.tracer(1)
+
+import time
+t = 60
+time.sleep(60)
+
+
